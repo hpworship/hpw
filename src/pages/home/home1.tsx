955c8a2cea822c2c98b263cb9e5a0289e26bc3f2
hpworship <hhPP1199@@>
2021-04-14 11:49:01 +0700

    add shortcurt tips

--- src/pages/home/home1.tsx
+++ src/pages/home/home1.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import cflogo from '../../imgs/cflogo.png';
+import iconI from '../../imgs/iconI.png';
+import iconA from '../../imgs/iconA.png';
 import './home1.css';
 import { Show } from '../show/show';
 import { Select } from '../select/select';
@@ -17,14 +19,18 @@ export const Home1 = () => {
   return (
     <div
       className="App"
-      onClick={(): void => {
-        if (screenState === 'init') setScreenState('select');
-      }}
+    // onClick={(): void => {
+    //   if (screenState === 'init') setScreenState('select');
+    // }}
     >
       {screenState === 'select' && <Select />}
       {screenState === 'init' && (
         <header className="App-header">
-          <img src={cflogo} className="App-logo" alt="logo" />
+          <div onClick={(): void => {
+            if (screenState === 'init') setScreenState('select');
+          }}>
+            <img src={cflogo} className="App-logo" alt="logo" />
+          </div>
           <p>HPW 12/04/2021</p>
           <p
             style={{
@@ -34,6 +40,15 @@ export const Home1 = () => {
           >
             120420210247
           </p>
+          <div style={{ flexDirection: 'row', alignItems: 'center', }}>
+            <img src={iconA} style={{ height: 25, width: 25, marginRight: 20 }} alt="iconAndroid" />
+            <a href="https://youtu.be/pbbf6HZcUCI" target="_blank" rel="noreferrer" style={{ color: 'white', fontSize: 18 }}>Tạo shortcut ngoài màn hình cho android</a>
+          </div>
+          <div style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <img src={iconI} style={{ height: 25, width: 25, marginRight: 20 }} alt="iconIos" />
+            <a href="https://youtu.be/i8Cz1B-x5qA" target="_blank" rel="noreferrer" style={{ color: 'white', fontSize: 18 }}>Tạo shortcut ngoài màn hình cho iPhone/iPad</a>
+          </div>
+
         </header>
       )}
       {screenState === 'play' && <Show {...state.selected.split(', ')} />}
